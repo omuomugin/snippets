2738483415df0af34eb3533b152099f579fafbe1
omuomugin <omu.omugin@gmail.com>
2020-11-26 14:24:11 +0900

    feat: add delete summary

--- git/log_basic.sh
+++ git/log_basic.sh
@@ -2,7 +2,11 @@
 ## commit xxxxxx (HEAD -> master)
 ## Author: omuomugin <omuomugin@out-box.dev>
 ## Date:   XXXXXXX
-## 
+##
 ## feat: add floating action button
 ## And file diff comes here
 git log --patch
+
+git log --oneline
+
+git log --oneline --diff-filter=D --summary
